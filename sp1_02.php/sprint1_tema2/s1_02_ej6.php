c67d9051eddd0fbd9f693d91d28ae2c2458487e9
Hebe <hebe_s@hotmail.com>
2025-09-26 11:20:51 +0200

    modifico el uso de random y agrego salida a la funcion

--- sp1_02.php/sprint1_tema2/s1_02_ej6.php
+++ sp1_02.php/sprint1_tema2/s1_02_ej6.php
@@ -1,15 +1,14 @@
 <?php
-function randomNumber($min, $max) {
-    return rand($min, $max);
+
+function isBetween(): bool {
+    $random = rand(0,1);
+    return $random === 1;
 }
-function isBetween($number, $true, $false) {
-    if($number >= 50)
-        return "Charlie me mordio el dedo!!, true";
-    else
-        return "Charlie me mordio el dedo!!, false";
-}
-$number = randomNumber(1, 100);
-echo "El número es: " . $number . "<br>";   
-echo isBetween($number, "true", "false");   
+
+if(isBetween()){
+    echo "Charlie te mordi el dedo!!";
+}else {
+    echo "Charlie no te mordio el dedo";
+}   
 
 ?>
