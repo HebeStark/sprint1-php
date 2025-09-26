b7de0ae59826350049770bb5898df871a1729fe4
Hebe <hebe_s@hotmail.com>
2025-09-26 11:47:42 +0200

    modifico el nombre de la funcion(verbo) paso parametro con tipo y devolucion

--- sp1_02.php/sprint1_tema3/s1_03_ej3.php
+++ sp1_02.php/sprint1_tema3/s1_03_ej3.php
@@ -1,15 +1,19 @@
 <?php
-function tieneElCaracter($nombres, $caracter) {
-    // Verifica si el carácter está en la cadena
-    return strpos($nombres, $caracter) !== false;
+function buscarCaracter(array $nombres, string $caracter): bool {
+    foreach ($nombres as $nombre) {//recorrere el array buscando el carecter que le indique con strpos
+        if (strpos($nombre, $caracter) === false) {
+            return false; //si una palabra no lo tiene me dovolvera false
+        }
+    }
+    return true; //si todas lo tienen me dovolvera true
 }
 
-$nombres = "Anna, Juan, Julia, María";
+$nombres = ["Anna", "Juan", "Julia", "María"];
 $caracter = "a";
-if (tieneElCaracter($nombres, $caracter)) {
+
+if (buscarCaracter($nombres, $caracter)) {
     echo "El carácter '$caracter' está presente en todos los nombres.";
 } else {
     echo "El carácter '$caracter' no está presente en todos los nombres.";
 }
-
 ?>
