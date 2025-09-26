26744c8956d10355b46edc78a3ce05fb7ff88a8e
Hebe <hebe_s@hotmail.com>
2025-09-26 11:10:49 +0200

    le pongo verbo a la funcion, tipo y retorno, y pequeñas modificaciones al if

--- sp1_02.php/sprint1_tema2/s1_02_ej5.php
+++ sp1_02.php/sprint1_tema2/s1_02_ej5.php
@@ -1,15 +1,15 @@
 <?php
-function grado($nota) {
+function calcularGrado( int $nota): String {
     if ($nota >= 60) {
         return "El grado deberia ser: Primera División";
-    } elseif ($nota >= 45 || $nota <= 59) {
+    } elseif ($nota >= 45) {
         return " El grado deberia ser: Segunda División";
-    } elseif ($nota >= 33 || $nota <= 44) {
+    } elseif ($nota >= 33) {
         return " El grado deberia ser: Tercera División";
-    } elseif ($nota > 33) {
+    } else {
         return "El estudiante reprobara";
     }     
 }   
-$nota = 65;
-echo  grado($nota);
+echo calcularGrado(60) . "<br>";
+echo calcularGrado(75) . "<br>";
 ?>
