5d5e276f94be628287a40053bb4290122f2e6af3
Hebe <hebe_s@hotmail.com>
2025-10-01 13:18:52 +0200

    modifico la funcion

--- sp1_02.php/sprint1_tema3/s1_03_ej3.php
+++ sp1_02.php/sprint1_tema3/s1_03_ej3.php
@@ -1,20 +1,15 @@
 <?php
-function buscarCaracter(array $nombres, string $caracter): bool {
-  foreach($nombres as $nombre){
-    $letras = str_split($nombre);
-        if(!in_array($caracter,$letras)) {
-        return false;
-        }
-    }  
-        return true;
+function buscarCaracter(array $nombres, string $caracter) {
+  foreach($nombres as $nombre){   
+        if(!str_contains($nombre,$caracter)) {
+            return "false";  
+        }else {
+            return "true";
+        }          
 }   
-
+}
 $nombres = ["Miguel", "Julian", "Laura"];
 $caracter = "u";
 
-if (buscarCaracter($nombres, $caracter)) {
-    echo "El carácter '$caracter' está presente en todos los nombres.";
-} else {
-    echo "El carácter '$caracter' no está presente en todos los nombres.";
-}
+echo buscarCaracter($nombres,$caracter);
 ?>
