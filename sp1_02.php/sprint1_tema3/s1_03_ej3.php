2f96591f7d7ff9bb11842291b2ac9eebad9c0cb9
Hebe <hebe_s@hotmail.com>
2025-10-01 13:39:12 +0200

    modificacion de respuesta de salida

--- sp1_02.php/sprint1_tema3/s1_03_ej3.php
+++ sp1_02.php/sprint1_tema3/s1_03_ej3.php
@@ -3,13 +3,19 @@ function buscarCaracter(array $nombres, string $caracter) {
   foreach($nombres as $nombre){   
         if(!str_contains($nombre,$caracter)) {
             return false;  
-        }else {
+        }
+    }
             return true;
-        }          
+                 
 }   
-}
+
 $nombres = ["Miguel", "Julian", "Laura"];
 $caracter = "u";
 
-echo buscarCaracter($nombres,$caracter);
+if(buscarCaracter($nombres,$caracter) == true){
+    echo "Todos los nombres contienen la letra " . $caracter ;
+}else {
+    echo "No todos los nombres contienen la letra: " . $caracter;
+}
+ ;
 ?>
