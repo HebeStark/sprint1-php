dc00df2b6e5b61144ade1a89fb2728eedb3a0052
Hebe <hebe_s@hotmail.com>
2025-10-03 15:33:39 +0200

    correccion de archivo

--- sprint1_tema6/ejercicio1/procesar.php
+++ sprint1_tema6/ejercicio1/procesar.php
@@ -1,15 +1,13 @@
 <?php 
 session_start();
+ $user  =$_POST['user'];
+ $email =$_POST['email'];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    $name  =$_POST['user'];
-    $email =$_POST['email'];
+       if (!empty($_POST['user']) && (!empty($_POST['email']))){
 
-    if (!empty($_POST['user']) && (!empty($_POST['email']))){
-
-        $_SESSION['user'] = $name;
+        $_SESSION['user'] = $user;
         $_SESSION['email'] = $email;
-
 
         echo  "Datos recibidos correctamente!" . "<br>";
         echo "Bienvenido/a: ". htmlspecialchars($_SESSION['user']) ."<br>";
