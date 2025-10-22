f2067315b355bf0b26e8057ae4129165f1b9965d
Hebe Stark <hebe_s@hotmail.com>
2025-10-22 18:32:04 +0200

    ejercicio 1-nivel2

--- sp1_02.php/sprint1_tema2/tema2_nivel2/nivel2_ej1.php
+++ sp1_02.php/sprint1_tema2/tema2_nivel2/nivel2_ej1.php
@@ -1,17 +1,18 @@
 <?php
-function aPagar($minutos){
- define("MINUTOS", 3);
- define("PRECIO", 10);
- $preMinAdi = 0.5; 
- $total = (PRECIO * $minutos) / MINUTOS; 
-     if($minutos == MINUTOS) {        
-       return $total;
-     }else if($minutos > MINUTOS) {
-         return $apagar = $total + ($minutos * $preMinAdi);             
-    } 
-    return $total;
-}
-    //llamada a la  funcion
+
+const PRECIO_BASE = 10;
+const MINUTOS_INCLUIDOS = 3;
+const PRECIO_MINUTO_ADICIONAL = 5;
+
+function aPagar(Int $minutos): Int {
+ if($minutos <= MINUTOS_INCLUIDOS){
+  return PRECIO_BASE;
+  } else {
+   $minutos_adicionales = $minutos - MINUTOS_INCLUIDOS;
+   $precio_total = PRECIO_BASE + ($minutos_adicionales * PRECIO_MINUTO_ADICIONAL);
+   return $precio_total;
+ }
+   }   //llamada a la  funcion
  $persona1 = 3;
  $persona2 = 10;
  $minutos1 = aPagar($persona1);
